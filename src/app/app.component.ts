b7a20a8ea8f1312774d69551eff26e3242c76bf4
Mukhraiya (EXT) <pd9rgf@splm.siemens.com>
2017-09-25 19:06:23 +0530

    First Commit for TODO Angular 2 Client

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'to-do',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'This is my todo list';
+}
